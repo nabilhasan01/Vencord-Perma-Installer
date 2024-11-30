fcb52c5869ab0e63c35803dc08f2800b048f6576
Nabil Hasan <nabil.hasan01@northsouth.edu>
2024-11-30 15:47:15 +0600

    Update Vencord-Install.cmd

--- Vencord-Install.cmd
+++ Vencord-Install.cmd
@@ -1,6 +1,6 @@
 @echo off
 vencord -update-self
 vencord -install -branch stable
-copy /V /Y "%programFiles%\Vencord\Vencord-Persist.cmd" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+copy /V /Y "C:\Vencord\Vencord-Persist.cmd" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
 %localappdata%\Discord\Update.exe --processStart Discord.exe
 exit
